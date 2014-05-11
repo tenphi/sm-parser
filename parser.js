860bd32e9a4a2855c508967c2087775da650eb22
Andrey Yamanov <tenphi@gmail.com>
2014-05-11 13:57:14 +0400

    change offset to ms & add parsing for note color & add output to file

--- parser.js
+++ parser.js
@@ -12,7 +12,7 @@ var keyMap = {
   'LYRICSPATH': 'lyricsPath',
   'CDTITLE': 'CDTitle',
   'MUSIC': 'music',
-  'OFFSET': ['offset', parseFloat],
+  'OFFSET': ['offset', parseOffset],
   'SAMPLESTART': ['sampleStart', parseFloat],
   'SAMPLELENGTH': ['sampleLength', parseFloat],
   'SELECTABLE': ['selectable', parseBoolean],
@@ -25,6 +25,10 @@ var keyMap = {
   'KEYSOUNDS': ['keySounds', parseKeySounds],
   'ATTACKS': ['attacks', parseAttacks]
 };
+
+function parseOffset(v) {
+  return parseFloat(v) * 1000;
+}
 
 function parseBoolean(v) {
   return v === 'YES' ? true : false;
@@ -71,7 +75,7 @@ function parseNotes(mode, what, difficulty, steps, what2, notes) {
     mode: mode,
     difficulty: difficulty,
     steps: parseInt(steps),
-    parsedNotes: notes.split(',').map(function(measure) {
+    rawNotes: notes.split(',').map(function(measure) {
       return measure.match(/.{1,4}/g);
     })
   };
@@ -80,11 +84,10 @@ function parseNotes(mode, what, difficulty, steps, what2, notes) {
 function parseMeasures(data) {
   var baseBPM = data.bpms[0][1];
   var measureLength = 60000 / baseBPM * 4;
-  console.log(data.notes);
   data.notes.forEach(function(difficulty) {
     var offset = 0;
     var notes = difficulty.notes = [];
-    difficulty.parsedNotes.forEach(function(measure, measureId) {
+    difficulty.rawNotes.forEach(function(measure, measureId) {
       var len = measure.length;
       var noteTime = measureLength / len;
       measure.forEach(function(note, i) {
@@ -93,13 +96,44 @@ function parseMeasures(data) {
           notes.push({
             offset: offset,
             steps: note,
-            measure: measureId
+            measure: measureId,
+            type: getStepType(i, len)
           });
         }
         offset += noteTime;
       });
     });
   });
+}
+
+// TODO: add support for triples
+function getStepType(offset, parts) {
+  var pow = 1, part;
+  // type 1 if there are 4 parts or less
+  if (parts / 4 <= 1) {
+    return 1;
+  } else {
+    // reduce offset by quads
+    parts = parts / 4;
+    while (offset >= parts) {
+      offset -= parts;
+    }
+  }
+  // type 1
+  if (offset === 0) {
+    return 1;
+  }
+  for (var i = 2; i < 5; i++) {
+    pow *= 2;
+    part = parts / pow;
+    if (part !== parseInt(part)) break;
+    if (offset - part === 0) {
+      return i;
+    } else if (offset - part > 0) {
+      offset -= part;
+    }
+  }
+  return 8;
 }
 
 var fs = require('fs');
@@ -135,4 +169,4 @@ inData.forEach(function(field) {
 });
 parseMeasures(outData);
 
-console.log(outData.notes[0].notes);
+fs.writeFileSync(outFile, JSON.stringify(outData), 'utf-8');
